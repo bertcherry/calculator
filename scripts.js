2cd96f65bfca792b22a31b86ed29e1f6d66fed06
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-02-15 13:00:34 -0800

    add numbers feed display value

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
+//Variable declarations
+var displayValue = "";
+const display = document.querySelector(".display");
+const numbers = document.querySelector(".numbers");
+const numberButtons = numbers.querySelectorAll("button");
+
+//Math functions for two number inputs
 function add(a, b) {
 	return a + b;
 };
@@ -21,3 +28,14 @@ function operate(operator) {
     if (operator === "/") {divide();};
     if (operator === "**") {power();};
 }
+
+//Store numbers into display
+function handleNumberClick() {
+    displayValue += this.id;
+    display.textContent = displayValue;
+}
+
+//Event listeners on number buttons to change display
+numberButtons.forEach((numberButton) => {
+    numberButton.addEventListener("click", handleNumberClick);
+});
